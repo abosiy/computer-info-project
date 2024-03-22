a6afd756a2f7939f0febed78ba744e50452f97d1
abosiy <142050775+abosiy@users.noreply.github.com>
2024-03-22 12:48:10 +0200

    Feat (#1)

---
+++ src/com/bosiy/MainApplication.java
@@ -0,0 +1,8 @@
+package com.bosiy;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        Computer apple = new Computer("Apple", "M1", 512);
+        System.out.println(apple);
+    }
+}

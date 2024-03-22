f7be3c1ce0fb17cdc7b27ccfdf84b2ba19df999f
abosiy1996 <abosiy1996@gmail.com>
2024-03-22 12:37:30 +0200

    [feed] - finished task

--- src/com/bosiy/MainApplication.java
+++ src/com/bosiy/MainApplication.java
@@ -2,7 +2,6 @@ package com.bosiy;
 
 public class MainApplication {
     public static void main(String[] args) {
-
         Computer apple = new Computer("Apple","M1", 512);
         System.out.println(apple);
     }
